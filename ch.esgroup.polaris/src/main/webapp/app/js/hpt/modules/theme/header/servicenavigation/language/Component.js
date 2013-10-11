3a4a9200e00693d8eb564414ef21fe7f8be26899
jbadeau <jose.badeau@gmail.com>
2013-10-11 18:08:17 +0200

    .

---
+++ ch.esgroup.polaris/src/main/webapp/app/js/hpt/modules/theme/header/servicenavigation/language/Component.js
@@ -0,0 +1,27 @@
+define([ 'hdk/mvc/view/ViewTemplate', 'text!./view/View.html' ], 
+function(ViewTemplate, template) {
+
+	function Component() {
+	}
+
+	Component.prototype = {
+
+		panel : null,
+
+		activate : function(parent) {
+			if (!this.panel) {
+				this.panel = new ViewTemplate(parent, template);
+			}
+			this.panel.show();
+		},
+
+		deactivate : function() {
+			if (this.panel) {
+				this.panel.hide();
+			}
+		}
+	};
+
+	return Component;
+
+});
