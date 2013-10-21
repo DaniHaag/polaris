35c607ec7ab08ff908138deb485ab471886a2698
jbadeau <jose.badeau@gmail.com>
2013-10-21 15:13:27 +0200

    .

---
+++ src/main/webapp/app/js/polaris/core/binding/ComponentBinding.js
@@ -0,0 +1,18 @@
+define([ 'knockout' ], function(ko) {
+
+	ko.bindingHandlers.component = {
+
+		init : function(element, valueAccessor) {
+			var $element = $(element), value = valueAccessor(), type = null;
+
+			if (type == "iframe") {
+			}
+			else if (type == "inline") {
+			}
+			else {
+			}
+		}
+
+	};
+
+});
